50b21553da6e980403593630daf6629c6390144b
Aijaz-Ahmed99 <aijazj990@gmail.com>
2025-10-19 15:05:57 +0500

    fix: safely truncate long passwords under 72 bytes for bcrypt

--- backend/data_handler.py
+++ backend/data_handler.py
@@ -109,7 +109,10 @@ def register_user(uuid: uuid_pkg.UUID, user: UserRegistration):
         # Encode to bytes, limit to 72 bytes
         password_bytes = raw_password.encode("utf-8")[:72]
 
-        hashed_password = bcrypt.hash(password_bytes)
+        safe_password = password_bytes.decode("utf-8", errors="ignore")
+
+        hashed_password = bcrypt.hash(safe_password)
+        return hashed_password
     
         # Save user data in the database
         cursor.execute("INSERT INTO users (user_id, user_name, email, password, registration_date) VALUES (%s, %s, %s, %s, %s)",
